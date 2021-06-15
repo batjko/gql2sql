e2be5f7e7e0f99fbdf54aff5eed0647cd12f56e6
Patrick Metzdorf <patrick.metzdorf@bigtelematics.com>
2021-06-16 00:34:07 +0100

    add more root query tests

--- src/schema/index.test.js
+++ src/schema/index.test.js
@@ -2,16 +2,22 @@ import test from 'ava'
 import { gql } from 'apollo-server'
 import { server } from '../server.js'
 
-const query = gql`
-  {
-    hello
-  }
-`
+import { typeDefs, resolvers } from './index.js'
 
-test('Root Query succeeds', async t => {
+test('Root Query succeeds without errors', async t => {
   const result = await server.executeOperation({ query })
 
   t.falsy(result.errors, 'Root Query operation returned errors.')
+})
+
+test('Root Query\'s "hello" query returns "World"', async t => {
+  const result = await server.executeOperation({ query })
+
+  const query = gql`
+    {
+      hello
+    }
+  `
   t.deepEqual(
     result.data.hello,
     'World',
@@ -19,8 +25,14 @@ test('Root Query succeeds', async t => {
   )
 })
 
-test('Sum numbers', t => {
-  const sum = 1 + 2
-  const expectedResult = 3
-  t.deepEqual(sum, expectedResult)
+test('typeDefs has at least two elements', t => {
+  t.true(
+    typeDefs.length >= 2,
+    "TypeDefs doesn't have at least Quyery and Mutation in it.",
+  )
+})
+
+test('resolvers should have Query and Mutation properties', t => {
+  t.truthy(resolvers.Query, "Root Resolver doesn't have Query property.")
+  t.truthy(resolvers.Mutation, "Root Resolver doesn't have Mutation property.")
 })
