3f44b40b14ee5364bd44e63365e2bf082f6633ad
Patrick Metzdorf <patrick.metzdorf@bigtelematics.com>
2021-06-18 11:47:40 +0100

    improve poem tests with mocking

--- src/providers/poems/poemProvider.js
+++ src/providers/poems/poemProvider.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
+const poemURL = 'https://www.poemist.com/api/v1/randompoems'
+
 export async function getRandomPoem() {
   return (await getSomePoems())[0]
 }
 
 export async function getSomePoems() {
-  const poem = await axios.get('https://www.poemist.com/api/v1/randompoems')
+  const poem = await axios.get(poemURL)
   return poem.data
 }
--- src/schema/poetry/poetrySchema.test.js
+++ src/schema/poetry/poetrySchema.test.js
@@ -1,8 +1,29 @@
 import test from 'ava'
 import { gql } from 'apollo-server'
-import { server } from '../../server.js'
 
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+
+import { server } from '../../server.js'
 import { poemResolvers } from './index.js'
+
+// Mock up the Poem API's response, so we avoid calling the real Poem API just for tests
+
+const mockPoem = {
+  title: 'testPoem',
+  content: 'Of all the things I have lost, I miss my mind the most.',
+  url: 'https://www.brainyquote.com/quotes/ozzy_osbourne_454746',
+  poet: {
+    name: 'Ozzy Osbourne',
+    url: 'https://en.wikipedia.org/wiki/Ozzy_Osbourne',
+  },
+}
+
+test.before(() => {
+  const mock = new MockAdapter(axios)
+
+  mock.onGet(/poem+/).reply(200, [mockPoem])
+})
 
 test('Poem Query succeeds without errors', async t => {
   const query = gql`
@@ -14,7 +35,25 @@ test('Poem Query succeeds without errors', async t => {
   `
   const result = await server.executeOperation({ query })
 
-  t.falsy(result.errors, 'Poem Query returned errors.')
+  t.falsy(result?.errors, 'Poem Query returned errors.')
+})
+
+test('Poem returns correct structure', async t => {
+  const query = gql`
+    {
+      poem {
+        title
+        content
+        url
+        poet {
+          name
+          url
+        }
+      }
+    }
+  `
+  const result = await server.executeOperation({ query })
+  t.deepEqual(result?.data?.poem, mockPoem, 'Poem response not as expected')
 })
 
 test('Poems Query succeeds without errors', async t => {
@@ -40,7 +79,7 @@ test('(Random) Poem Query returns single Poem', async t => {
   `
   const result = await server.executeOperation({ query })
 
-  t.truthy(result.data.poem, 'Poem result is not returned')
+  t.truthy(result?.data?.poem, 'Poem result is not returned')
 })
 
 test('Poems Query returns array', async t => {
@@ -53,19 +92,19 @@ test('Poems Query returns array', async t => {
   `
   const result = await server.executeOperation({ query })
 
-  t.truthy(Array.isArray(result.data.poems), 'Poems not being returned')
+  t.truthy(Array.isArray(result?.data?.poems), 'Invalid result for Poems query')
 })
 
 test('Poem resolvers include poem and poems functions', t => {
-  t.truthy(poemResolvers.Query.poem, 'Poem Resolver missing')
+  t.truthy(poemResolvers?.Query?.poem, 'Poem Resolver missing')
   t.true(
-    typeof poemResolvers.Query.poem === 'function',
+    typeof poemResolvers?.Query?.poem === 'function',
     'Poem resolver is not a function',
   )
 
-  t.truthy(poemResolvers.Query.poems, 'Poems Resolver missing')
+  t.truthy(poemResolvers?.Query?.poems, 'Poems Resolver missing')
   t.true(
-    typeof poemResolvers.Query.poems === 'function',
+    typeof poemResolvers?.Query?.poems === 'function',
     'Poems resolver is not a function',
   )
 })
